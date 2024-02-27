c28b95691f0d9f2713ae019a91a119e091b0d2d8
Mboehmlaender <41646409+Mboehmlaender@users.noreply.github.com>
2024-02-27 20:29:05 +0100

    Update
    Directories and firmware

--- Firmware/vial91/config.h
+++ Firmware/vial91/config.h
@@ -1,31 +1,24 @@
-// Copyright 2022 Victor Lucachi (@victorlucachi)
+// Copyright 2024 Michael Böhmländer
+//
+// Original Design, Code and Build by Victor Lucachi Copyright 2022
+// https://github.com/victorlucachi
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #pragma once
 
-/*
- * Feature disable options
- *  These options are also useful to firmware size reduction.
- */
-
-/* disable debug print */
-//#define NO_DEBUG
-
-/* disable print */
-//#define NO_PRINT
-
-/* disable action features */
-//#define NO_ACTION_LAYER
-//#define NO_ACTION_TAPPING
-//#define NO_ACTION_ONESHOT
-
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
 #define DEBOUNCE 5
 
+/* Encoders */
 #define ENCODERS_PAD_A { B1, B2 }
 #define ENCODERS_PAD_B { F7, B3 }
+
+/* 8 layers */
 #define DYNAMIC_KEYMAP_LAYER_COUNT 8
+
+/* 32 macros */
 #define DYNAMIC_KEYMAP_MACRO_COUNT 32
 
+/* LED configuration */
 #define WS2812_DI_PIN D7
 #define RGBLED_NUM 6
